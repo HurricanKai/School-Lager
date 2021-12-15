67643e0807cee0fc64179ae4f6e56bad976e236a
Kai Jellinghaus <contact@kaij.tech>
2021-12-15 07:10:29 +0100

    Transactions main, TODO: View transactions

---
+++ Lager/Shared/TransactionDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Lager.Shared;
+
+public sealed record TransactionDTO(TransactionType Type, int Count, string ArticleId, string Note);
+
+public enum TransactionType
+{
+    Addition,
+    Removal,
+    Buying
+}
